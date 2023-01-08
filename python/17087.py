9edd7b5342d4592a26780dfe5803fac6ae296ac9
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-08 22:08:17 +0900

    python/17087.py 4(clear)

--- python/17087.py
+++ python/17087.py
@@ -2,23 +2,10 @@ def gcd(x: int, y: int) -> int:
     return gcd(y, x % y) if y else x
 
 
-def find_index(ls: list, x: int) -> int:
-    idx = 0
-    for i in range(len(position_list) - 1):
-        if ls[i + 1] > x:
-            return i
-    return len(position_list) - 1
-
-
 n, s = map(int, input().split())
-position_list = sorted(list(map(int, input().split())))
-position_list = [position_list[0]] + position_list
-idx = find_index(position_list, s)
-if idx == 0 and len(position_list) > 2:
-    print(
-        gcd(position_list[idx + 1] - s, position_list[idx + 2] - position_list[idx + 1])
-    )
-elif idx == len(position_list) - 1:
-    print(gcd(s - position_list[idx], position_list[idx] - position_list[idx - 1]))
-else:
-    print(gcd(s - position_list[idx], position_list[idx + 1] - s))
+position_list = list(map(int, input().split()))
+result = abs(s - position_list[0])
+for i in range(1, len(position_list)):
+    tmp = abs(s - position_list[i])
+    result = gcd(result, tmp)
+print(result)
