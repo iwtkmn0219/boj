cc98a9b99ff9839e803b9c35cc6a9164c773a55e
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-08 22:06:48 +0900

    python/17087.py 2

--- python/17087.py
+++ python/17087.py
@@ -14,4 +14,9 @@ n, s = map(int, input().split())
 position_list = sorted(list(map(int, input().split())))
 position_list = [position_list[0]] + position_list
 idx = find_index(position_list, s)
-print(gcd(s - position_list[idx], position_list[idx + 1] - s))
+if idx == 0 and len(position_list) > 2:
+    print(
+        gcd(position_list[idx + 1] - s, position_list[idx + 2] - position_list[idx + 1])
+    )
+else:
+    print(gcd(s - position_list[idx], position_list[idx + 1] - s))
