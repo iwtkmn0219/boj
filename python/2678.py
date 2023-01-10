67f31e2585ec965f7fad7c7ae77e0dba238ba1fb
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-10 22:39:26 +0900

    python/2678.py 2(clear)

--- python/2678.py
+++ python/2678.py
@@ -24,24 +24,23 @@ def bin_to_float(s: str) -> float:
     ]
     if not minus:
         for i in range(1, len(s)):
-            if s[i] == "1":
-                result += 1 / pow[i - 1]
+            if s[i] == '1':
+                result += 1/pow[i - 1]
         return result
     else:
-        tmp_s = ""
+        tmp_s = ''
         for i in range(len(s) - 1, -1, -1):
-            if s[i] == "1":
-                tmp_s += "0"
+            if s[i] == '1':
+                tmp_s += '0'
                 s = s[:i]
                 for j in range(len(tmp_s) - 1, -1, -1):
                     s += tmp_s[j]
-                print("S", s)
                 break
-            tmp_s += "1"
-
+            tmp_s += '1'
+        
         for i in range(1, len(s)):
-            if s[i] == "0":
-                result += 1 / pow[i - 1]
+            if s[i] == '0':
+                result += 1/pow[i - 1]
         return -result
 
 
@@ -55,25 +54,25 @@ p = int(input())
 for tc in range(p):
     c, d, s = input().split()
     d = int(d)
-    if c == "?" and d == 0 and s == "F":
-        print("-1.0")
+    if c == '?' and d == 0 and s == 'F':
+        print('-1.0')
         continue
     tmp = dic[c] + f"{bin(d)[2:]:0>11}"
     if s == "D":
         tmp += "1"
     else:
         tmp += "0"
-    answer = f"{bin_to_float(tmp):.16f}"
-    new_answer = ""
-
+    answer = f'{bin_to_float(tmp):.16f}'
+    new_answer = ''
+    
     zero_checker = True
     for i in range(len(answer) - 1, 0, -1):
-        if answer[i - 1] == ".":
+        if answer[i - 1] == '.':
             new_answer += answer[i]
             continue
-
+        
         if zero_checker:
-            if answer[i] == "0":
+            if answer[i] == '0':
                 continue
             else:
                 new_answer += answer[i]
@@ -81,7 +80,7 @@ for tc in range(p):
         else:
             new_answer += answer[i]
     new_answer += answer[0]
-
+    
     for i in range(len(new_answer) - 1, -1, -1):
-        print(new_answer[i], end="")
+        print(new_answer[i], end='')
     print()
