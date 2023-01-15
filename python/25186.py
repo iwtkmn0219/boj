726e941caeaf8e43ce7eb7852df15291ec07db5b
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-15 12:17:48 +0900

    python/25186.py 2

--- python/25186.py
+++ python/25186.py
@@ -2,13 +2,16 @@ n = int(input())
 clothes = list(map(int, input().split()))
 closet = {}
 
-happy = True
-for c in clothes:
-    if c not in closet:
-        closet[c] = 1
-    else:
-        closet[c] += 1
-        if closet[c] > n // 2:
-            happy = False
-            break
-print("Happy" if happy else "Unhappy")
+if n == 1:
+    print("Happy")
+else:
+    happy = True
+    for c in clothes:
+        if c not in closet:
+            closet[c] = 1
+        else:
+            closet[c] += 1
+            if closet[c] > n // 2:
+                happy = False
+                break
+    print("Happy" if happy else "Unhappy")
