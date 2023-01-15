55fb4ba0e14488f2bec489b8841f51b47f52f8d9
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-15 12:25:03 +0900

    python/25186.py 6(clear)

--- python/25186.py
+++ python/25186.py
@@ -1,6 +1,6 @@
 n = int(input())
 clothes = list(map(int, input().split()))
-if n == 1:
+if n == 1 and clothes[0] == 1:
     print("Happy")
 else:
     print("Happy" if (max(clothes) <= sum(clothes) // 2) else "Unhappy")
