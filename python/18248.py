f9f3b4c6dba294996cc73b0d84b1afce9ca167d7
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-16 20:44:22 +0900

    python/18248.py 4(advanced clear)

--- python/18248.py
+++ python/18248.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-ls = [[0] * m for _ in range(n)]
+dp = [[0] * m for _ in range(n)]
 possible = True
 for i in range(n):
     for j in range(m):
@@ -9,21 +9,21 @@ for i in range(n):
 
         if i == 0:
             if a[i][j] == 1:
-                ls[i][j] = 1
+                dp[i][j] = 1
             else:
-                ls[i][j] = 2
+                dp[i][j] = 2
             continue
 
         if j < m - 1:
-            if ls[i - 1][j] > ls[i - 1][j + 1]:
+            if dp[i - 1][j] > dp[i - 1][j + 1]:
                 if a[i][j] == 1 and a[i][j + 1] == 0:
                     possible = False
-            if ls[i - 1][j] < ls[i - 1][j + 1]:
+            if dp[i - 1][j] < dp[i - 1][j + 1]:
                 if a[i][j] == 0 and a[i][j + 1] == 1:
                     possible = False
 
         if a[i][j] == 0:
-            ls[i][j] = ls[i - 1][j] + 2
+            dp[i][j] = dp[i - 1][j] + 2
         else:
-            ls[i][j] = ls[i - 1][j] + 1
+            dp[i][j] = dp[i - 1][j] + 1
 print("YES" if possible else "NO")
