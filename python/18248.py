e26ccd8e0c033e0c392f0f198d8a588121253e0a
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-16 20:42:25 +0900

    python/18248.py 1

---
+++ python/18248.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+ls = [[0] * m for _ in range(n)]
+print(ls)
+
+possible = True
+for i in range(n):
+    for j in range(m):
+        if not possible:
+            break
+
+        if i == 0:
+            if a[i][j] == 1:
+                ls[i][j] = 0
+            else:
+                ls[i][j] = 1
+            continue
+
+        if j < m - 1:
+            if ls[i - 1][j] > ls[i - 1][j + 1]:
+                if a[i][j] == 1 and a[i][j + 1] == 0:
+                    possible = False
+            if ls[i - 1][j] < ls[i - 1][j + 1]:
+                if a[i][j] == 0 and a[i][j + 1] == 1:
+                    possible = False
+
+        if a[i][j] == 0:
+            ls[i][j] += 1
+print("YES" if possible else "NO")
