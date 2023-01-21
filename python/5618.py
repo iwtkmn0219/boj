bfb507037cbf761a1dd0e975ade72eeb539adde1
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-21 21:32:13 +0900

    python/5618.py 2

--- python/5618.py
+++ python/5618.py
@@ -1,11 +1,12 @@
+from math import gcd
 n = int(input())
 if n == 2:
     a, b = map(int, input().split())
-    for i in range(1, min(a, b)):
+    for i in range(1, gcd(a, b) + 1):
         if a % i == 0 and b % i == 0:
             print(i)
 else:
     a, b, c = map(int, input().split())
-    for i in range(1, min(a, b, c)):
+    for i in range(1, gcd(a, gcd(b, c)) + 1):
         if a % i == 0 and b % i == 0 and c % i == 0:
             print(i)
