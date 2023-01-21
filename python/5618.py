43b055392c085fcc9ae72a169c79c61a733d8ea3
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-21 21:32:42 +0900

    python/5618.py 3(clear)

--- python/5618.py
+++ python/5618.py
@@ -1,12 +1,24 @@
-from math import gcd
+from math import gcd, sqrt
+
+def div(n: int) -> list:
+
+    tmp = []
+    for i in range(1, int(sqrt(n)) + 1):
+        if (n % i == 0):
+            tmp.append(i) 
+            if ( (i**2) != n) : 
+                tmp.append(n // i)
+    tmp.sort()
+    return tmp
+
 n = int(input())
 if n == 2:
     a, b = map(int, input().split())
-    for i in range(1, gcd(a, b) + 1):
-        if a % i == 0 and b % i == 0:
-            print(i)
+    ls = div(gcd(a, b))
+    for element in ls:
+        print(element)
 else:
     a, b, c = map(int, input().split())
-    for i in range(1, gcd(a, gcd(b, c)) + 1):
-        if a % i == 0 and b % i == 0 and c % i == 0:
-            print(i)
+    ls = div(gcd(a, gcd(b, c)))
+    for element in ls:
+        print(element)
