91a3018b84de78d95398d1164c87dcada718cc1e
iwtkmn0219 <iwtkmn0219@naver.com>
2023-01-25 13:41:26 +0900

    python/1672.py 2

--- python/1672.py
+++ python/1672.py
@@ -19,8 +19,13 @@ DNA_DIC = {
 
 
 n = int(input())
-s = input()
+s = list(input())
+dp = [s] + [[""] * n for _ in range(n - 1)]
 
-for _ in range(n - 1):
-    s = s[:-2] + DNA_DIC[s[-2:]]
-print(s)
+for i in range(1, n):
+    for j in range(n - i):
+        if j == n - i - 1:
+            dp[i][j] = DNA_DIC[dp[i - 1][j] + dp[i - 1][j + 1]]
+            continue
+        dp[i][j] = dp[i - 1][j]
+print(dp[n - 1][0])
