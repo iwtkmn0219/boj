7d076f13d1841655aca1d1c2a0cbeb351a4cf183
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-01 20:01:45 +0900

    python/25088.py 1

---
+++ python/25088.py
@@ -0,0 +1,14 @@
+t = int(input())
+for tc in range(t):
+    row, col = map(int, input().split())
+    print(f"사례 #{tc + 1}:")
+    for r in range(2 * row + 1):
+        if r == 0:
+            print(".." + "+-" * (col - 1) + "+")
+        elif r == 1:
+            print(".." + "|-" * (col - 1) + "|")
+        else:
+            if r % 2 == 0:
+                print("+-" * col + "+")
+            else:
+                print("|-" * col + "|")
