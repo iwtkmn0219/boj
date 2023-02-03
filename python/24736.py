e61404f2a0557d5a6db9beae03423ffb91008e3f
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-03 22:59:57 +0900

    python/24736.py 1(clear)

---
+++ python/24736.py
@@ -0,0 +1,4 @@
+als = list(map(int, input().split()))
+bls = list(map(int, input().split()))
+print(als[0] * 6 + als[1] * 3 + als[2] * 2 + als[3] + als[4] * 2, end=" ")
+print(bls[0] * 6 + bls[1] * 3 + bls[2] * 2 + bls[3] + bls[4] * 2)
