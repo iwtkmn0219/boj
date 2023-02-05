1feca53208952dc11d590d4204917a42f79871d2
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-06 03:30:02 +0900

    python/27245.py 6(clear)

--- python/27245.py
+++ python/27245.py
@@ -1,12 +1,7 @@
 w = int(input())
 l = int(input())
 h = int(input())
-if min(w, l) > h:
-    condition1 = min(w, l) >= 2 * h
-else:
-    condition1 = h >= 2 * min(w, l)
-condition2 = max(w, l) <= 2 * min(w, l)
-if condition1 and condition2:
-    print("good")
-else:
-    print("bad")
+
+a = min(w, l)
+b = max(w, l)
+print("good" if a * 2 >= b and a >= h * 2 else "bad")
