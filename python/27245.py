b1f311d863ab57cef4b99787eca69cc17d99ce81
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-06 03:29:51 +0900

    python/27245.py 5

--- python/27245.py
+++ python/27245.py
@@ -2,10 +2,10 @@ w = int(input())
 l = int(input())
 h = int(input())
 if min(w, l) > h:
-    condition1 = min(w, l) / h >= 2
+    condition1 = min(w, l) >= 2 * h
 else:
-    condition1 = h / min(w, l) >= 2
-condition2 = max(w, l) / min(w, l) <= 2
+    condition1 = h >= 2 * min(w, l)
+condition2 = max(w, l) <= 2 * min(w, l)
 if condition1 and condition2:
     print("good")
 else:
