dda7a93657718b919b2ddf1d53b76dea39f6b700
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-09 14:39:33 +0900

    python/4458.py 2(clear)

--- python/4458.py
+++ python/4458.py
@@ -1,6 +1,4 @@
 n = int(input())
 for _ in range(n):
-    line = input().split()
-    for _, e in enumerate(line):
-        print(e[0].upper() + e[1:], end=" ")
-    print()
+    line = input()
+    print(line[0].upper() + line[1:])
