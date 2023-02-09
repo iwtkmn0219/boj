d9b39fecd8b408af61f926949162ba4c238d916b
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-10 04:12:55 +0900

    python/12790.py 2(clear)

--- python/12790.py
+++ python/12790.py
@@ -5,7 +5,7 @@ for _ in range(n):
     )
     total_hp = max(hp + alpha_hp, 1)
     total_mp = max(mp + alpha_mp, 1)
-    total_atk = max(atk + alpha_atk, 1)
-    total_dif = max(dif + alpha_dif, 1)
+    total_atk = max(atk + alpha_atk, 0)
+    total_dif = dif + alpha_dif
 
     print(total_hp + 5 * total_mp + 2 * total_atk + 2 * total_dif)
