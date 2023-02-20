1163e13aafdabfa0ab19e29ac20107224579aafb
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-21 08:58:14 +0900

    python/25165.py 1

---
+++ python/25165.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+a, d = map(int, input().split())
+r, c = map(int, input().split())
+direction = [1, -1]
+d_idx = 0 if d == 1 else 1
+x = a - 1
+y = 0
+monster = False
+while x < m and y < n:
+    xx = x + direction[d_idx]
+    if 0 <= xx < m:
+        x = xx
+    else:
+        d_idx = (d_idx + 1) % 2
+        y += 1
+    if x == c and y == r:
+        monster = True
+        break
+if monster:
+    print("NO...")
+else:
+    print("YES!")
