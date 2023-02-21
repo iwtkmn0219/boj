76418d9d487143568ca6e80d98df8f342c59a7b0
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-21 10:26:51 +0900

    python/2495.py 1

---
+++ python/2495.py
@@ -0,0 +1,16 @@
+for _ in range(3):
+    line = input()
+    if len(line) == 1:
+        print(1)
+        continue
+    prev = line[0]
+    maximum = 0
+    cnt = 1
+    for i in range(1, len(line)):
+        if line[i] == prev:
+            cnt += 1
+        else:
+            maximum = max(maximum, cnt)
+            cnt = 1
+            prev = line[i]
+    print(maximum)
