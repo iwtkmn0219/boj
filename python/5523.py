5a370c245ef7601e05ca5aa9f3fde01752a3efd8
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-21 10:50:24 +0900

    python/5523.py 1

---
+++ python/5523.py
@@ -0,0 +1,9 @@
+n = int(input())
+result = [0, 0]
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a > b:
+        result[0] += 1
+    elif a < b:
+        result[1] += 1
+print(*result)
